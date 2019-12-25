2ba3ba50616966e60a4227136b45f260e5f6876a
msuleman2001 <msuleman2001@gmail.com>
2019-12-25 16:49:01 +0500

    Initial commit

---
+++ DrawingSample.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Painter
+{
+    partial class DrawingSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnArc = new System.Windows.Forms.Button();
+            this.btnCurve = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnText = new System.Windows.Forms.Button();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.btnPolygon = new System.Windows.Forms.Button();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(12, 12);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(64, 40);
+            this.btnLine.TabIndex = 0;
+            this.btnLine.Text = "Line";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(12, 58);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(64, 40);
+            this.btnCircle.TabIndex = 1;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnArc
+            // 
+            this.btnArc.Location = new System.Drawing.Point(12, 196);
+            this.btnArc.Name = "btnArc";
+            this.btnArc.Size = new System.Drawing.Size(64, 40);
+            this.btnArc.TabIndex = 2;
+            this.btnArc.Text = "Arc";
+            this.btnArc.UseVisualStyleBackColor = true;
+            this.btnArc.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnCurve
+            // 
+            this.btnCurve.Location = new System.Drawing.Point(12, 242);
+            this.btnCurve.Name = "btnCurve";
+            this.btnCurve.Size = new System.Drawing.Size(64, 40);
+            this.btnCurve.TabIndex = 4;
+            this.btnCurve.Text = "Curve";
+            this.btnCurve.UseVisualStyleBackColor = true;
+            this.btnCurve.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(12, 104);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(64, 40);
+            this.btnRectangle.TabIndex = 5;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnText
+            // 
+            this.btnText.Location = new System.Drawing.Point(12, 288);
+            this.btnText.Name = "btnText";
+            this.btnText.Size = new System.Drawing.Size(64, 40);
+            this.btnText.TabIndex = 6;
+            this.btnText.Text = "Text";
+            this.btnText.UseVisualStyleBackColor = true;
+            this.btnText.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(12, 334);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(64, 40);
+            this.btnImage.TabIndex = 7;
+            this.btnImage.Text = "Image";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // btnPolygon
+            // 
+            this.btnPolygon.Location = new System.Drawing.Point(12, 150);
+            this.btnPolygon.Name = "btnPolygon";
+            this.btnPolygon.Size = new System.Drawing.Size(64, 40);
+            this.btnPolygon.TabIndex = 8;
+            this.btnPolygon.Text = "Polygon";
+            this.btnPolygon.UseVisualStyleBackColor = true;
+            this.btnPolygon.Click += new System.EventHandler(this.DrawShape);
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocation.Location = new System.Drawing.Point(635, 9);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(0, 25);
+            this.lblLocation.TabIndex = 9;
+            // 
+            // DrawingSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 388);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.btnPolygon);
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.btnText);
+            this.Controls.Add(this.btnRectangle);
+            this.Controls.Add(this.btnCurve);
+            this.Controls.Add(this.btnArc);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnLine);
+            this.Name = "DrawingSample";
+            this.Text = "DrawingSample";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DrawingSample_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnArc;
+        private System.Windows.Forms.Button btnCurve;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnText;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.Button btnPolygon;
+        private System.Windows.Forms.Label lblLocation;
+    }
+}
---
+++ DrawingSample.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Painter
+{
+    public partial class DrawingSample : Form
+    {
+        Graphics graphics_context;
+        Pen outline;
+        SolidBrush fill_color;
+
+        //Constructor of Form
+        public DrawingSample()
+        {
+            InitializeComponent();
+            graphics_context = this.CreateGraphics();
+            outline = new Pen(Color.Red);
+            fill_color = new SolidBrush(Color.Green);
+        }
+
+        //Show Location of Mouse Pointer on label
+        private void DrawingSample_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblLocation.Text = "X: " + e.X.ToString() + "," + "Y:" + e.Y.ToString();
+        }
+
+        //Drawing shapes
+        public void DrawShape(object sender, EventArgs e)
+        {
+            Button btnShape = (Button)sender;
+            switch (btnShape.Text)
+            {
+                case "Line":
+                    graphics_context.DrawLine(outline, 150, 10, 250, 100);
+                    break;
+                case "Circle":
+                    graphics_context.DrawEllipse(outline, 300, 10, 50, 50);
+                    break;
+                case "Rectangle":
+                    graphics_context.DrawRectangle(outline, 300, 150, 50, 50);
+                    break;
+                case "Polygon":
+                    Point[] points = new Point[5];
+                    points[0] = new Point(150, 120);
+                    points[1] = new Point(120, 180);
+                    points[2] = new Point(190, 180);
+                    points[3] = new Point(200, 241);
+                    points[4] = new Point(214, 180);
+                    graphics_context.DrawPolygon(outline, points);
+                    break;
+                case "Arc":
+                    Rectangle rect = new Rectangle(450, 50, 100, 100);
+                    graphics_context.DrawArc(outline, rect, 90,100);
+                    break;
+                case "Curve":
+                    Point[] points_curve = new Point[5];
+                    points_curve[0] = new Point(250, 220);
+                    points_curve[1] = new Point(220, 280);
+                    points_curve[2] = new Point(290, 280);
+                    points_curve[3] = new Point(300, 341);
+                    points_curve[4] = new Point(314, 280);
+                    graphics_context.DrawCurve(outline, points_curve);
+                    break;
+                case "Text":
+                    Font font = new Font("Verdana", 20);
+                    Point location = new Point(400, 200);
+                    StringFormat draw_format = new StringFormat();
+                    draw_format.FormatFlags = StringFormatFlags.DirectionVertical;
+                    graphics_context.DrawString("hello world!", font, fill_color, location, draw_format);
+                    break;
+                case "Image":
+                    PictureBox picture_box = new PictureBox();
+                    picture_box.Location = new Point(550, 100);
+                    picture_box.Size = new Size(200, 250);
+                    picture_box.BorderStyle = BorderStyle.FixedSingle;
+                    picture_box.SizeMode = PictureBoxSizeMode.StretchImage;
+                    picture_box.Image = Image.FromFile(@"E:\ImageBank\icon\png1\014-book.png");
+                    this.Controls.Add(picture_box);
+                    break;
+            }
+        }
+    }
+}
